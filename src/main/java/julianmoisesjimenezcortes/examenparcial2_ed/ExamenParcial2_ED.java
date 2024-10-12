cbed5f83f679ba446c4fdd244ae342dedf7a3219
JulianDarkstar <julimoijimenezz@gmailcom>
2024-10-12 08:27:49 -0600

    Changes have been made to main file

--- src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/ExamenParcial2_ED.java
+++ src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/ExamenParcial2_ED.java
@@ -11,6 +11,6 @@ package julianmoisesjimenezcortes.examenparcial2_ed;
 public class ExamenParcial2_ED {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Se hicieron cambios");
     }
 }
