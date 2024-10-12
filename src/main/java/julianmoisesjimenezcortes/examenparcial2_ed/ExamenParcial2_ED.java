e02718b802bbcca417a63e6b835dc002070ea121
JulianDarkstar <julimoijimenezz@gmailcom>
2024-10-12 07:33:38 -0600

    This is the first commit of the proyect

---
+++ src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/ExamenParcial2_ED.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package julianmoisesjimenezcortes.examenparcial2_ed;
+
+/**
+ *
+ * @authors Julián Moisés Jiménez Cortés, Jesus Rodriguez Romero, Christopher Joey Vidal Cervantes
+ */
+public class ExamenParcial2_ED {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
