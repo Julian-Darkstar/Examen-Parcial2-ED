1253b112adcae5c196cfb96ca26fe69b6f82bd07
JulianDarkstar <julimoijimenezz@gmailcom>
2024-10-13 10:09:09 -0600

    Se creo el inicio del menu del juego en la clase main y se corrigieron errores en la clase Ronda

--- src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/ExamenParcial2_ED.java
+++ src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/ExamenParcial2_ED.java
@@ -8,6 +8,37 @@ public class ExamenParcial2_ED {
 
     public static void main(String[] args) {
         
+        System.out.println(" __       __                                  __                                          __    __                        __                         \n" +
+"/  \\     /  |                                /  |                                        /  |  /  |                      /  |                        \n" +
+"$$  \\   /$$ |  ______   _______    _______  _$$ |_     ______    ______    _______       $$ |  $$ | __    __  _______   _$$ |_     ______    ______  \n" +
+"$$$  \\ /$$$ | /      \\ /       \\  /       |/ $$   |   /      \\  /      \\  /       |      $$ |__$$ |/  |  /  |/       \\ / $$   |   /      \\  /      \\ \n" +
+"$$$$  /$$$$ |/$$$$$$  |$$$$$$$  |/$$$$$$$/ $$$$$$/   /$$$$$$  |/$$$$$$  |/$$$$$$$/       $$    $$ |$$ |  $$ |$$$$$$$  |$$$$$$/   /$$$$$$  |/$$$$$$  |\n" +
+"$$ $$ $$/$$ |$$ |  $$ |$$ |  $$ |$$      \\   $$ | __ $$    $$ |$$ |  $$/ $$      \\       $$$$$$$$ |$$ |  $$ |$$ |  $$ |  $$ | __ $$    $$ |$$ |  $$/ \n" +
+"$$ |$$$/ $$ |$$ \\__$$ |$$ |  $$ | $$$$$$  |  $$ |/  |$$$$$$$$/ $$ |       $$$$$$  |      $$ |  $$ |$$ \\__$$ |$$ |  $$ |  $$ |/  |$$$$$$$$/ $$ |      \n" +
+"$$ | $/  $$ |$$    $$/ $$ |  $$ |/     $$/   $$  $$/ $$       |$$ |      /     $$/       $$ |  $$ |$$    $$/ $$ |  $$ |  $$  $$/ $$       |$$ |      \n" +
+"$$/      $$/  $$$$$$/  $$/   $$/ $$$$$$$/     $$$$/   $$$$$$$/ $$/       $$$$$$$/        $$/   $$/  $$$$$$/  $$/   $$/    $$$$/   $$$$$$$/ $$/       \n" +
+"                                                                                                                                                     \n" +
+"                                                                                                                                                     ");
+       
+        System.out.println("\t\t _____ __   _ _______ _______  ______ _     _ _______ _______ _____  _____  __   _ _______ _______\n" +
+"\t\t   |   | \\  | |______    |    |_____/ |     | |       |         |   |     | | \\  | |______ |______\n" +
+"\t\t __|__ |  \\_| ______|    |    |    \\_ |_____| |_____  |_____  __|__ |_____| |  \\_| |______ ______|");
+        
+        
+        System.out.println("\n\t\t\t\t -Selecciona un tipo de partida: Partida rapida o Partida Normal\n"
+                          +"\t\t\t\t -Se generaran un armamento aleatorio de 5 armas e iran apareciendo \n"
+                          +"\t\t\t\t  armas en cada ronda por las que podras intercambiar en tu armamento,  \n"
+                          + "\t\t\t\t usarlas en el momento o guardarlas si tienes espacio en tu arsenal\n"
+                          + "\t\t\t\t -Cada ronda tendra un numero de monstruos que aparecen, pero unicamente\n"
+                          + "\t\t\t\t  puedes ver a 3 a la vez, asi que ten cuidado y usa tus armas sabiamente.\n"
+                          + "\t\t\t\t -Tu personaje tiene una cantidad de vida limitada y los monstruos pueden atacarte,\n"
+                          + "\t\t\t\t  si la vida de tu personaje llega a 0 la partida termina\n"
+                          + "\t\t\t\t - Para ganar la partida, debes eliminar a todos los monstruos de todas las rondas\n"
+                          + "\t\t\t\tantes de que tu vida llegue a 0\n\n"
+                          + "\t\t\t\t\t\t Buena suerte cazador, la necesitaras.");
+        
+        System.out.println("¿INICIAR PARTIDA?");
+        /**
         //Generacion de inventario aleatorio
         int IDArma;
         Armas NuevaArma;
@@ -16,11 +47,12 @@ public class ExamenParcial2_ED {
         for(int i=0; i<4; i++){
         IDArma = (int)(Math.random()*5+1);
          NuevaArma = new Armas(IDArma);
-            System.out.println("Arma generada" + NuevaArma.nombre);
+            System.out.println("Arma generada " + NuevaArma.nombre);
         A1.agregarAInventario(NuevaArma);
         }
         A1.mostrarArmamento();
         
+        //Generacion de colas de monstruos
         ColaMonstruos cola = new ColaMonstruos();
         int vidaUsuario = 100; 
 
@@ -33,6 +65,7 @@ public class ExamenParcial2_ED {
 
         cola.mostrarMonstruos();
 
+        //Sistema de ataques
         System.out.println("\nAtacando a los monstruos en orden:");
 
         while (!cola.colaMonstruos.isEmpty() && vidaUsuario > 0) {
@@ -53,6 +86,6 @@ public class ExamenParcial2_ED {
             System.out.println("El usuario ha derrotado a todos los monstruos y le queda " + vidaUsuario + " de vida.");
         } else {
             System.out.println("El usuario ha sido derrotado por los monstruos.");
-        }
+        }**/
     }
 }
--- src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/Ronda.java
+++ src/main/java/julianmoisesjimenezcortes/examenparcial2_ed/Ronda.java
@@ -18,21 +18,32 @@ public class Ronda {
     int NumRonda = 0;
     private int tope;  // Simula el tope de la pila
 
+    //Constructor para el numero de rondas
     public Ronda(int numeroDeRondas, Jugador jugador, Monstruos[] monstruos, int NumRonda) {
         this.numeroDeRondas = numeroDeRondas;
         this.jugador = jugador;
         this.monstruos = monstruos;
         this.tope = -1;  // La pila empieza vacía
     }
-    while (NumRonda <= numeroDeRondas){
-    // Empuja (push) una ronda a la "pila"
-    public void pushRonda(Monstruos monstruo) {
-        if (tope < monstruos.length - 1) {
-            tope++;
-            monstruos[tope] = monstruo;
-            System.out.println("Se añade a la pila el monstruo: " );
-        } else {
-            System.out.println("No se puede añadir más monstruos, pila llena.");
+    
+    public void iniciarRondas(Monstruos Monstruo) {
+        int NumRonda = 0;
+        while (NumRonda < numeroDeRondas) {
+            // Aquí puedes añadir la lógica que deseas ejecutar en cada ronda
+            pushRonda(Monstruo);
+            NumRonda++;
+        }
+    }
+    
+       
+         // Empuja (push) una ronda a la "pila"
+        public void pushRonda(Monstruos monstruo) {
+            if (tope < monstruos.length - 1) {
+                 tope++;
+                monstruos[tope] = monstruo;
+                System.out.println("Se añade a la pila el monstruo: " );
+            } else {
+                System.out.println("No se puede añadir más monstruos, pila llena.");
         }
     }
 
@@ -49,5 +60,5 @@ public class Ronda {
     }
 }
 
-}
+
    
